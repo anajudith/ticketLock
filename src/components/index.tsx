af3dd96129498ebc495a43dc188a792bf09bfb20
Ana <ana-judth@hotmail.com>
2023-12-05 15:20:36 -0300

    🚧 create file for export components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,21 @@
 import BasicDatePicker from "./BasicDatePicker";
+import BasicSelect from "./BasicSelect";
 import CardShows from "./CardShows";
 import Form from "./Form";
 import Header from "./Header";
+import ShowDetails from "../pages/ShowDetailsPage";
+import InputFileUpload from "./InputFileUpload";
+import MultipleSelect from "./MultipleSelect";
+import SimpleSelect from "./SimpleSelect";
 
-export { Form, Header, BasicDatePicker, CardShows };
+export {
+  Form,
+  Header,
+  BasicDatePicker,
+  CardShows,
+  BasicSelect,
+  ShowDetails,
+  InputFileUpload,
+  MultipleSelect,
+  SimpleSelect,
+};
